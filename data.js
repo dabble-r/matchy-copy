70ccf368eef3adadb554d70b357e20483316faed
dabble-r <njbroussard.1@gmail.com>
2024-04-02 15:54:50 -0500

    functions_completed2

--- data.js
+++ data.js
@@ -95,7 +95,8 @@ function getRandom(animals) {
 }
 console.log(friends);
 
-animals[0] = {...animal,'friends': friends};
+friends.push(animals[getRandom(animals)]['name']);
+animals[0]['friends'] = friends;
 
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
